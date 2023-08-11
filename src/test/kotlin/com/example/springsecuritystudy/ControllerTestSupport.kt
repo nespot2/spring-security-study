6dcc67949b4a717becf7c788c31c0ba3c11d9288
leejh <nespot2@gmail.com>
2023-08-12 04:59:05 +0900

    ControllerTestSupport 수정

--- src/test/kotlin/com/example/springsecuritystudy/ControllerTestSupport.kt
+++ src/test/kotlin/com/example/springsecuritystudy/ControllerTestSupport.kt
@@ -1,5 +1,6 @@
 package com.example.springsecuritystudy
 
+import com.example.springsecuritystudy.config.SecurityConfig
 import com.example.springsecuritystudy.controller.MainController
 import com.example.springsecuritystudy.controller.UserDetailServiceConfig
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
@@ -13,7 +14,7 @@ import org.springframework.context.annotation.Import
         MainController::class,
     ]
 )
-@Import(UserDetailServiceConfig::class)
+@Import(UserDetailServiceConfig::class, SecurityConfig::class)
 abstract class ControllerTestSupport {
 
 }
